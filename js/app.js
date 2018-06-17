965d9682ac9df21f6008184e387f8b8fb4a9a4a1
ik9999 <ik9999@users.noreply.github.com>
2018-06-17 10:55:39 +0300

    remove backbone history start/end

--- js/app.js
+++ js/app.js
@@ -8,35 +8,6 @@
   var $checkIcon = $('<i class="icon-check"></i>');
 
   var init = function() {
-      //var template = '   <li>  '  + 
-    //'     <div class="btn-group navbar-btn js-labelstyle-list-dropdown dropdown">  '  + 
- //'       <button class="btn btn-default btn-sm js-board-labelstyle" type="button"><i class="icon-tag"></i></button>  '  + 
-    //'       <button data-toggle="dropdown" class="btn btn-default btn-sm dropdown-toggle active js-board-labelstyle" type="button"> <span><i class="icon-caret-down cur"></i></span> <span class="sr-only">Toggle Dropdown</span> </button>  '  + 
-    //'       <ul class="dropdown-menu arrow arrow-right col-xs-push-0">  '  + 
-    //'         <li class="js-labelstyle-popup js-dropdown-popup dropdown-popup col-xs-12">  '  + 
-    //'           <div class="clearfix text-center"><a class="js-back-to-board-labelstyle hide pull-left" href="#"><i class="icon-caret-left"></i></a><span class="col-xs-10"><strong>Change Labels style</strong></span><a class="js-close-popover pull-right" href="#"><i class="icon-remove"></i></a>  '  + 
-    //'           </div>  '  + 
-    //'         </li>  '  + 
-    //'         <li class="col-xs-12 divider js-labelstyle-list"></li>  '  + 
-    //'         <li class="col-xs-12 btn-block text-justify"><a href="#" name="private" class="  h6 js-set-labelstyle-default navbar-btn"> <span class="show text-primary navbar-btn h5"> Default  </span> </a> </li>  '  + 
-    //'         <li class="col-xs-12 btn-block text-justify"> <a href="#" name="public" class=" h6 navbar-btn js-set-labelstyle-tags"><span class="show text-primary navbar-btn h5"> Tags  </span> </a> </li>  '  + 
-    //'       </ul>  '  + 
-    //'     </div>  '  + 
-    //'  </li>  ' ;  
-    //var $toolbar = $('.hdrpvtvws');
-    //var $toolbarItem = $(template);
-    //$btnByType['default'] = $toolbarItem.find('.js-set-labelstyle-default');
-    //$btnByType['tags'] = $toolbarItem.find('js-set-labelstyle-tags');
-    //$btnByType[type].addClass('btn-default').children('span').append($checkIcon);
-    //$toolbar.prepend($toolbarItem);
-    //$.each($btnByType, (btntype, $btn) => {
-      //$btn.on('click', function() {
-        //type = btntype;
-        //Backbone.history.stop(); 
-        //Backbone.history.start(); 
-      //});
-    //});
-
     var oldCardView = App.CardView;
     var newCardView = oldCardView.extend({
       template: function(opts) {
@@ -74,8 +45,6 @@
       }
     });
     App.CardView = newCardView;
-    Backbone.history.stop(); 
-    Backbone.history.start(); 
   };
 
   $dc.ready(function() {
